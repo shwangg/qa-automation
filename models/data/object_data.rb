878741bb01cffbc90ae629ad3203b11e33b05151
Paul Farestveit <pfarestveit@berkeley.edu>
2019-02-20 12:05:50 -0800

    Move shared object data elements

--- models/data/object_data.rb
+++ models/data/object_data.rb
@@ -9,7 +9,30 @@ class ObjectData
   end
 
   DATA = [
-      OBJECT_NUM = new('objectNumber')
+      BRIEF_DESCRIPS = new('briefDescriptions'),
+      BRIEF_DESCRIP = new('briefDescription'),
+      COLLECTION = new('collection'),
+      COMMENTS = new('comments'),
+      COMMENT = new('comment'),
+      NUM_OBJECTS = new('numberOfObjects'),
+      OBJ_NAME_CURRENCY = new('objectNameCurrency'),
+      OBJ_NAME_GRP = new('objectNameGroup'),
+      OBJ_NAME_LANG = new('objectNameLanguage'),
+      OBJ_NAME_LEVEL = new('objectNameLevel'),
+      OBJ_NAME_NAME = new('objectName'),
+      OBJ_NAME_NOTE = new('objectNameNote'),
+      OBJ_NAME_SYSTEM = new('objectNameSystem'),
+      OBJ_NAME_TYPE = new('objectNameType'),
+      OBJECT_NUM = new('objectNumber'),
+      RESPONSIBLE_DEPTS = new('responsibleDepartments'),
+      RESPONSIBLE_DEPT = new('responsibleDepartment'),
+      TITLE = new('title'),
+      TITLE_GRP = new('titleGroup'),
+      TITLE_LANG = new('titleLanguage'),
+      TITLE_TYPE = new('titleType'),
+      TITLE_TRANSLATION = new('titleTranslation'),
+      TITLE_TRANSLATION_LANG = new('titleTranslationLanguage'),
+      TITLE_TRANSLATION_SUB_GRP = new('titleTranslationSubGroup')
   ]
 
 end
