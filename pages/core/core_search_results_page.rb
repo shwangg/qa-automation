fa67be3ba7c959ccbf4b67bc2e0629ac3c125eee
Paul Farestveit <pfarestveit@berkeley.edu>
2019-02-22 10:39:18 -0800

    Move more shared UI up the hierarchy

--- pages/core/core_search_results_page.rb
+++ pages/core/core_search_results_page.rb
@@ -8,17 +8,4 @@ class CoreSearchResultsPage < SearchResultsPage
 
   DEPLOYMENT = Deployment::CORE
 
-  # Checks for the presence of a search results row containing the object number from a test data set
-  # @param [Hash] data_set
-  # @return [boolean]
-  def object_row_exists?(data_set)
-    exists? result_row(data_set[CoreObjectData::OBJECT_NUM.name])
-  end
-
-  # Clicks a search results row containing the object number from a test data set
-  # @param [Hash] data_set
-  def click_result(data_set)
-    wait_for_element_and_click result_row(data_set[CoreObjectData::OBJECT_NUM.name])
-  end
-
 end
--- pages/supers/search_results_page.rb
+++ pages/supers/search_results_page.rb
@@ -16,4 +16,17 @@ class SearchResultsPage
     wait_until(Config.short_wait) { elements(result_rows).any? }
   end
 
+  # Checks for the presence of a search results row containing the object number from a test data set
+  # @param [Hash] data_set
+  # @return [boolean]
+  def object_row_exists?(data_set)
+    exists? result_row(data_set[ObjectData::OBJECT_NUM.name])
+  end
+
+  # Clicks a search results row containing the object number from a test data set
+  # @param [Hash] data_set
+  def click_result(data_set)
+    wait_for_element_and_click result_row(data_set[ObjectData::OBJECT_NUM.name])
+  end
+
 end
