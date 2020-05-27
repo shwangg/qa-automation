c616ee02e1cdcd4bce93a6cb19a34a320568d778
cesarvh <cesarv.h@berkeley.edu>
2020-05-27 00:09:51 -0700

    CC-1140: Added admin page

--- models/data/core/core_tools/core_invocables_data.rb
+++ models/data/core/core_tools/core_invocables_data.rb
@@ -1,6 +1,7 @@
 class CoreInvocablesData < CollectionSpaceData
 
   DATA = [
+      # Invocable Doc Type
       INVOCABLE_NAME = new('name'),
       INVOCABLE_DESC = new('notes'),
       INVOCABLE_NO_CONTEXT = new('supportsNoContext'),
@@ -11,15 +12,21 @@ class CoreInvocablesData < CollectionSpaceData
       INVOCABLE_DOC_TYPE = new('forDocType'),
       INVOCABLE_RUNS_ON_PANEL = new('', 'Runs on'),
 
+      # Batch Job
       INVOCABLE_BATCH_CREATES_NEW_FOCUS = new('createsNewFocus', 'Navigate to new record when complete'),
       INVOCABLE_BATCH_LIST_PANEL = new('', 'Data Updates'),
       INVOCABLE_BATCH_CLASSNAME = new('className', 'Java class'),
       
-
-
+      # Reports
       INVOCABLE_REPORT_OUTPUT_MIME = new('outputMIME', 'For record type'),
       INVOCABLE_REPORT_FILENAME = new('filename'),
-      INVOCABLE_REPORT_LIST_PANEL = new('', 'Reports')
+      INVOCABLE_REPORT_LIST_PANEL = new('', 'Reports'),
+
+      # Report & Batch roles
+      REPORTER_AND_EDITOR_ROLE = new("CAN_EDIT_CAN_RUN"),
+      REPORT_EDITOR_ROLE = new("CAN_EDIT_CANT_RUN"),
+      REPORT_INVOKER_ROLE = new("CANT_EDIT_CAN_RUN"),
+      NO_REPORT_PERMISSIONS_ROLE = new("NO_REPORT_PERMISSIONS"),
   ]
 
 end
