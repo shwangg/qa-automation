48e5fc5f95a373134dcd442f8348bf511d17ae2a
Seanhaoxiang <seanhaoxiang@gmail.com>
2020-07-31 13:32:58 -0700

    data_test

--- pages/core/core_procedures/core_loan_in_page.rb
+++ pages/core/core_procedures/core_loan_in_page.rb
@@ -6,16 +6,7 @@ class CoreLoanInPage
   include Page
   include CollectionSpacePages
   include CoreSidebar
-<<<<<<< HEAD
-<<<<<<< HEAD
-  include CoreExhibitionInfoForm
 
-  DEPLOYMENT = Deployment::CORE
-
-end
-=======
-=======
->>>>>>> 04ee7d97cc5e0a786fa7c2aa24f059c9ad1ab6f7
   include CorePages
   include CoreLoanInInfoForm
 
@@ -43,7 +34,3 @@ end
   end
 
 end
-<<<<<<< HEAD
->>>>>>> 38e746f... completed test
-=======
->>>>>>> 04ee7d97cc5e0a786fa7c2aa24f059c9ad1ab6f7
